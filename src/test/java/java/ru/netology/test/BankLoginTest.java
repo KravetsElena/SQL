f36dec1784cd6341644c46d182d7d32a5a18599e
ElenaKr <Kravets_2024@internet.ru>
2023-09-04 11:00:31 +0500

    Initial commit

---
+++ src/test/java/java/ru/netology/test/BankLoginTest.java
@@ -0,0 +1,53 @@
+package java.ru.netology.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.ru.netology.data.DataHelper;
+import java.ru.netology.data.SQLHelper;
+import java.ru.netology.page.LoginPage;
+
+import static java.ru.netology.data.SQLHelper.cleanDatabase;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BankLoginTest {
+
+    @AfterAll
+    static void teardown(){
+        cleanDatabase();
+    }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exit login and password from sut test data")
+    void shouldSuccessfulLogin(){
+        var loginPage  = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    @DisplayName("Should get error notification if user is not exist in base")
+    void shouldGetErrorNotificationIfLoginWithRandomUserWithoutAddingToBase(){
+        var loginPage  = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotificationVisibility();
+    }
+
+    @Test
+    @DisplayName("Should get error notification if login with in base and active user and random verification code")
+    void shouldGetErrorNotificationIfLoginWithInBaseAndActiveUserAndRandomVerificationCode(){
+        var loginPage  = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = DataHelper.generateRandomCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotificationVisibility();
+
+    }
+}
